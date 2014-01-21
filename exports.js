430ff408fd286495632a97f4958563e0ad890985
Leo Giovanetti <hello@leog.me>
2014-01-21 07:25:30 -0800

    Create exports.js

---
+++ exports.js
@@ -0,0 +1,8 @@
+define({
+    load: function (name, req, onload, config) {
+        //req has the same API as require().
+        req([name], function (value) {
+            onload(value);
+        });
+    }
+});
